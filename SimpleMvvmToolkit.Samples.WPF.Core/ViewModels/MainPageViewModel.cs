713d9d25a388aafab3df9c89985fcc1adcebf742
Anthony Sneed <tony@tonysneed.com>
2019-11-20 14:58:14 -0600

    Add models, services, view models, views.

---
+++ SimpleMvvmToolkit.Samples.WPF.Core/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SimpleMvvmToolkit.Express;
+
+namespace SimpleMvvmToolkit.Samples.WPF.Core.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase<MainPageViewModel>
+    {
+        public event EventHandler<NotificationEventArgs<Exception>> ErrorNotice;
+
+        private string _bannerText = "Hello Simple MVVM Toolkit";
+        public string BannerText
+        {
+            get => _bannerText;
+            set
+            {
+                _bannerText = value;
+                NotifyPropertyChanged(m => m.BannerText);
+            }
+        }
+
+        // Helper method to notify View of an error
+        private void NotifyError(string message, Exception error)
+        {
+            ErrorNotice?.Invoke(this, new NotificationEventArgs<Exception>(message, error));
+        }
+    }
+}
---
+++ SimpleMvvmToolkit.Samples.WPF.Core/Views/CustomerView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using SimpleMvvmToolkit.Samples.WPF.Core.ViewModels;
+
+namespace SimpleMvvmToolkit.Samples.WPF.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for CustomerView.xaml
+    /// </summary>
+    public partial class CustomerView : UserControl
+    {
+        public CustomerView(CustomerViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
